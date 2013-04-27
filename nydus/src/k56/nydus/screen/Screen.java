18ff77b9ace6df836d9acdfc48a4580d1f842efd
Kjetil Mehl <aspic@mehl.no>
2013-04-27 11:32:35 +0200

    Added camera, batch and some testing.

--- nydus/src/k56/nydus/screen/Screen.java
+++ nydus/src/k56/nydus/screen/Screen.java
@@ -1,13 +1,37 @@
 package k56.nydus.screen;
 
 import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.TextureRef;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector3;
 
 public class Screen implements ApplicationListener, InputProcessor {
+	private int viewWidth = 10;
+	private int viewHeight = 10;
+	
+	private OrthographicCamera camera;
+	private SpriteBatch batch;
+	
+	private Texture texture;
+	private TextureRegion region;
+	
+	private Vector3 clicked = new Vector3();
 
 	@Override
 	public void create() {
+		float aspect = (float)Gdx.graphics.getWidth()/Gdx.graphics.getHeight();
+		camera = new OrthographicCamera(viewWidth*aspect, viewHeight);
+		batch = new SpriteBatch();
+		batch.setProjectionMatrix(camera.combined);
 		
+		region = new TextureRegion(new Texture(Gdx.files.internal("assets/test.png")));
 	}
 
 	@Override
@@ -18,26 +42,27 @@ public class Screen implements ApplicationListener, InputProcessor {
 
 	@Override
 	public void render() {
-		// TODO Auto-generated method stub
+		// Clear screen
+		Gdx.graphics.getGL20().glClearColor(0, 1, 1, 1);
+		Gdx.graphics.getGL20().glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
+		batch.begin();
+		batch.draw(region, 0, 0);
+		// engine.render()
+		batch.end();
 		
 	}
 
 	@Override
 	public void pause() {
-		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void resume() {
-		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void dispose() {
-		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
@@ -60,7 +85,10 @@ public class Screen implements ApplicationListener, InputProcessor {
 
 	@Override
 	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-		// TODO Auto-generated method stub
+		clicked.set(screenX, screenY, 0);
+		camera.unproject(clicked);
+		// engine.clicked();
+		
 		return false;
 	}
 
